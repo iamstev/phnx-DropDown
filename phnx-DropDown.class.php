ab26e1389bf6645790c3d8aefebfba84c85ade06
Stev <topshelfsmith@gmail.com>
2015-02-03 17:41:46 -0500

    Return instead of print
    This now returns that HTML instead of printing it. Should be more
    versatile.

--- phnx-DropDown.class.php
+++ phnx-DropDown.class.php
@@ -7,117 +7,126 @@
 		var $style;
 		var $disabled;
 		var $selected;
-		
+		var $html;
+
 		function listoptions(){
-		
-		
+
+
 			/*	Style : 0 through 9
 				Code  : 0thru9
 			*/
 			if($this->style == '0thru9'){
 				for ($i = 0; $i <= 9; $i++) {
 					if($i == $this->selected){
-						print '<option value="'.$i.'" selected="selected">'.$i.'</option>';
+						$this->html .= '<option value="'.$i.'" selected="selected">'.$i.'</option>';
 					}else{
-						print '<option value="'.$i.'">'.$i.'</option>';
+						$this->html .= '<option value="'.$i.'">'.$i.'</option>';
 					}
 				}
 			}
-			
-			
+
+
 			/*	Style : 1 through 9
 				Code  : 1thru9
 			*/
 			if($this->style == '1thru9'){
 				for ($i = 1; $i <= 9; $i++) {
 					if($i == $this->selected){
-						print '<option value="'.$i.'" selected="selected">'.$i.'</option>';
+						$this->html .= '<option value="'.$i.'" selected="selected">'.$i.'</option>';
 					}else{
-						print '<option value="'.$i.'">'.$i.'</option>';
+						$this->html .= '<option value="'.$i.'">'.$i.'</option>';
 					}
 				}
 			}
-		
-		
+
+
 			/*	Style : 1 through 31
 				Code  : 1thru31
 			*/
 			if($this->style == '1thru31'){
 				for ($i = 1; $i <= 31; $i++) {
 					if($i == $this->selected){
-						print '<option value="'.$i.'" selected="selected">'.$i.'</option>';
+						$this->html .= '<option value="'.$i.'" selected="selected">'.$i.'</option>';
 					}else{
-						print '<option value="'.$i.'">'.$i.'</option>';
+						$this->html .= '<option value="'.$i.'">'.$i.'</option>';
 					}
 				}
 			}
-			
-			
+
+
 			/*	Style : 1 through 12 leading zeros
 				Code  : 1thru12
 			*/
 			if($this->style == '1thru12'){
 				for ($i = 1; $i <= 12; $i++) {
 					if($i == $this->selected){
-						print '<option value="'.sprintf('%02s',$i).'" selected="selected">'.sprintf('%02s',$i).'</option>';
+						$this->html .= '<option value="'.sprintf('%02s',$i).'" selected="selected">'.sprintf('%02s',$i).'</option>';
 					}else{
-						print '<option value="'.sprintf('%02s',$i).'">'.sprintf('%02s',$i).'</option>';
+						$this->html .= '<option value="'.sprintf('%02s',$i).'">'.sprintf('%02s',$i).'</option>';
 					}
 				}
 			}
-			
-			
+
+
 			/*	Style : 12 then 1 through 11 leading zeros
 				Code  : 12hour
 			*/
 			if($this->style == '12hour'){
 				if($this->selected == '12'){
-					print '<option value="12" selected="selected">12</option>';
+					$this->html .= '<option value="12" selected="selected">12</option>';
 				}else{
-					print '<option value="12">12</option>';
+					$this->html .= '<option value="12">12</option>';
 				}
 				for ($i = 1; $i <= 11; $i++) {
 					if($i == $this->selected){
-						print '<option value="'.sprintf('%02s',$i).'" selected="selected">'.sprintf('%02s',$i).'</option>';
+						$this->html .= '<option value="'.sprintf('%02s',$i).'" selected="selected">'.sprintf('%02s',$i).'</option>';
 					}else{
-						print '<option value="'.sprintf('%02s',$i).'">'.sprintf('%02s',$i).'</option>';
+						$this->html .= '<option value="'.sprintf('%02s',$i).'">'.sprintf('%02s',$i).'</option>';
 					}
 				}
 			}
-			
+
 			/*	Style : 0 through 59 leading zeros
 				Code  : 0thru59
 			*/
 			if($this->style == '0thru59'){
 				for ($i = 0; $i <= 59; $i++) {
 					if($i == $this->selected){
-						print '<option value="'.sprintf('%02s',$i).'" selected="selected">'.sprintf('%02s',$i).'</option>';
+						$this->html .= '<option value="'.sprintf('%02s',$i).'" selected="selected">'.sprintf('%02s',$i).'</option>';
 					}else{
-						print '<option value="'.sprintf('%02s',$i).'">'.sprintf('%02s',$i).'</option>';
+						$this->html .= '<option value="'.sprintf('%02s',$i).'">'.sprintf('%02s',$i).'</option>';
 					}
 				}
 			}
-		
-			
+
+
 		}
-		
-		function display(){
-			print'<select';
+
+		function display($opt = NULL){
+			$this->html = '<select';
 			if(isset($this->name)){
-				print' name="'.$this->name.'"';
+				$this->html .= ' name="'.$this->name.'"';
 			}
 			if(isset($this->id)){
-				print' id="'.$this->id.'"';
+				$this->html .= ' id="'.$this->id.'"';
 			}
 			if(isset($this->classes)){
-				print' class="'.$this->classes.'"';
+				$this->html .= ' class="'.$this->classes.'"';
 			}
 			if($this->disabled === TRUE){
-				print' disabled';
+				$this->html .= ' disabled';
 			}
-			print'>';
+			$this->html .= '>';
 			$this->listoptions();
-			print'</select>';
+			$this->html .= '</select>';
+
+			if($opt === 'print'){
+				print $this->html;
+			}else{
+				return $this->html;
+			}
+
 		}
+
 	}
 ?>
